a47e50067e50860d1dd3c349ad868cd28757cfab
otsohellman <otso.hellman@gmail.com>
2020-10-21 22:54:57 +0300

    add ui build

---
+++ build/precache-manifest.9173ef5b1fad7718e83cca85bafbbf45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afffafdb41b54c901296bc9b3817c464",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e5720e987599a9bbb7f",
+    "url": "/static/js/2.c52f314d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.c52f314d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d954ae70611882c6676b",
+    "url": "/static/js/main.817db54c.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
