c6056d590afdd8b52bb822326ea30ce21768f54e
otsohellman <otso.hellman@gmail.com>
2020-10-21 21:50:37 +0300

    3-4

--- index.js
+++ index.js
@@ -26,11 +26,11 @@ let persons = [
   }
 ]
 
-app.get('/info', (req,res) => {
+app.get('/info', (req, res) => {
   res.send(
     `<p>Phonebook has info of ${persons.length} people</p>
     <p>${new Date(Date.now()).toLocaleString()}</p>`
-    )
+  )
 })
 
 app.get('/api/persons', (req, res) => {
@@ -40,8 +40,20 @@ app.get('/api/persons', (req, res) => {
 app.get('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
   const personObject = persons.find(person => person.id === id)
-  
+
   personObject ? res.json(personObject) : res.status(404).end()
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const personObject = persons.find(person => person.id === id)
+
+  if (personObject) {
+    persons = persons.filter(person => person.id !== id)
+    res.status(204).end()
+  } else {
+    res.status(404).end()
+  }
 })
 
 const PORT = 3001
