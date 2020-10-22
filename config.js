fe7fc1cada0f904b3dcb5bafb71c9451621ddce4
otsohellman <otso.hellman@gmail.com>
2020-10-22 22:47:59 +0300

    3-12

---
+++ config.js
@@ -0,0 +1,6 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+const DBPassword = process.env.DBPASSWORD
+
+export default DBPassword
---
+++ mongo.js
@@ -0,0 +1,45 @@
+import DBPassword from './config.js'
+import mongoose from 'mongoose'
+
+
+
+if (process.argv.length < 3) {
+    console.log("no password given")
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+//const url = `mongodb+srv://fullstack:${DBPassword}@cluster0.8gjok.mongodb.net/puhelinluettelo-app?retryWrites=true&w=majority`
+const url = `mongodb+srv://fullstack:${password}@cluster0.8gjok.mongodb.net/puhelinluettelo-app?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 3) {
+    Person.find({}).then(persons => {
+        console.log('Phonebook')
+        persons.forEach(person => console.log(`${person.name} ${person.number}`))
+        mongoose.connection.close()
+    })
+} else {
+    const name = process.argv[3]
+    const number = process.argv[4]
+    const personObject = new Person({
+        name,
+        number,
+    })
+    personObject.save().then(res => {
+        console.log(`Added ${name} number ${number} to phonebook`)
+        console.log(res)
+        mongoose.connection.close()
+    })
+}
+
